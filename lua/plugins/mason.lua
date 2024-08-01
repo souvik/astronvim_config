0f1f108ac21e89dc4a56878b7804a0cc67eef24a
Souvik Kumar Khan <souvik.khan@ymail.com>
2024-08-01 01:05:01 -0500

    Ensuring rubocop's installation

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,6 +9,7 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
+        "rubocop"
         -- add more arguments for adding more language servers
       },
     },
@@ -20,6 +21,7 @@ return {
     opts = {
       ensure_installed = {
         "stylua",
+        "rubocop"
         -- add more arguments for adding more null-ls sources
       },
     },
